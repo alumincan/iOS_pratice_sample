0bf580805dee9c8442c59fb732acc6d01357af63
alucard <alumincan@gmail.com>
2022-03-21 17:04:39 +0800

    add Class 2 practice image vinew used

---
+++ Class2/Class2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class2
+//
+//  Created by Alucard on 2022/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
